5bf4507f2aa815156b274898926f293a8b0a72df
Dreyk <guninalexander@gmail.com>
2018-01-30 21:47:26 +0300

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 
 def main(_):
     # Creates a graph.
+    print('Source b')
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c = tf.matmul(a, b)
