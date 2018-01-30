a082a9e60ac402dc9af8a379da2e5e85b2863c69
Dreyk <guninalexander@gmail.com>
2018-01-30 21:47:49 +0300

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 
 def main(_):
+    print('Source master')
     # Creates a graph.
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
